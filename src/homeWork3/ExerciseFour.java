113afff4d52b8f7032ed543b2e88b060fee62945
EvgeniyHotetovskiy <squaer.r@mail.ru>
2025-02-16 13:31:14 +0300

    homeWork3

---
+++ src/homeWork3/ExerciseFour.java
@@ -0,0 +1,16 @@
+package homeWork3;
+
+public class ExerciseFour {
+    //Даны значения: x=5; y=2; c=x*y;
+    // Расставьте операции инкремента декремента так, чтобы после выполнения операции (c=x*y) с равнялось 12, x=равнялось 6, y=1Выведите полученные значения в консоль.
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        x++;
+        y--;
+        int c = x * y;
+        System.out.println("Значение x: " + x);
+        System.out.println("Значение y: " + y);
+        System.out.println("Значение c: " + c);
+    }
+}
---
+++ src/homeWork3/ExerciseTwo.java
@@ -0,0 +1,14 @@
+package homeWork3;
+
+public class ExerciseTwo {
+    //x=x+5; y=y*9; z=z-6; Необходимо укоротить данные выражения, чтобы результат не изменился.
+    // x=8; y=9; z=12; (операции присваивания, нужно просто написать по-другому)
+    static int x;
+    static int y;
+    static int z;
+    public static void main(String[] args) {
+        x += 5;
+        y *= 9;
+        z -= 6;
+    }
+}
